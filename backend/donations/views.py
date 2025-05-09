2f4bbab20a25abf7fac1f9efa32f3a1c1787d20b
Jiwon Kwon <kjiwon411@gmail.com>
2025-05-09 14:18:53 +0900

    feat: add serilasers

--- backend/donations/views.py
+++ backend/donations/views.py
@@ -1,115 +1,14 @@
-from drf_yasg import openapi
 from drf_yasg.inspectors import SwaggerAutoSchema
 from drf_yasg.utils import swagger_auto_schema
-from rest_framework.decorators import api_view
-import json
 import datetime
-from django.http import JsonResponse
-from django.db.models import Q
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
-from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import action
+
 from .serializers import CreateDonationRequestSerializer, DonationRequestIdSerializer, DonationRequestSerializer, DonatorRegisteredIdSerializer, MatchRequestSerializer, MessageSerializer, RejectedMatchRequestSerializer, SelectedMatchRequestSerializer
-from .models import DonationRequest, SelectedMatchRequest, RejectedMatchRequest
-
-
-# @swagger_auto_schema(method='post', responses={201: DonationRequestIdSerializer})
-# @api_view(['POST'])
-# def create(request):
-#     """
-#     Create a donation request
-#     """
-#     # TODO: When donation due date is past today, return 400
-
-#     request_data = json.loads(request.POST.get('request_data'))
-    
-#     donation_request = DonationRequest.objects.create(
-#         requester_id=request.user,
-#         **request_data
-#     )
-    
-#     # Handle image upload if present
-#     if 'image' in request.FILES:
-#         donation_request.image = request.FILES['image']
-#         donation_request.save()
-        
-#     return JsonResponse({
-#         'id': donation_request.id,
-#     }, status=201)
-        
-# @swagger_auto_schema(method='get', responses={200: DonationRequestSerializer})
-# @api_view(['GET'])
-# def detail(request, id):
-#     """
-#     Get a donation request by id
-#     """
-#     donation_request = DonationRequest.objects.get(id=id)
-#     return JsonResponse(donation_request, status=200)
-
-
-# @swagger_auto_schema(method='post', responses={200: MatchRequestSerializer})
-# @api_view(['POST'])
-# def match(request, ):
-#     """
-#     Match a donation request to a donator
-#     """
-
-#     if request.method == 'POST':
-#         # TODO: Consider AST rather than JSON
-#         data = json.loads(request.body)
-#     else:
-#         data = request.POST
-    
-
-#     # if not request.user.is_authenticated:
-#     #     return JsonResponse({
-#     #         'status': 'error',
-#     #         'message': 'Authentication required'
-#     #     }, status=401)
-    
- 
-#     query = Q()
-    
-#     if 'blood_type' in data:
-#         query &= Q(blood_type=data['blood_type'])
-    
-#     if 'sex' in data:
-#         query &= Q(sex=data['sex'])
-    
-#     if 'location' in data:
-#         query &= Q(location=data['location'])
-    
-#     # TODO: Improve age filter
-#     if 'age' in data:
-#         age = int(data['age'])
-#         age_min = age - 5  # For example, 5 years younger
-#         age_max = age + 5  # For example, 5 years older
-#         query &= Q(age__gte=age_min, age__lte=age_max)
-    
-#     if 'next_donation_date' in data:
-#         next_donation_date = datetime.strptime(data['next_donation_date'], '%Y-%m-%d').date()
-#         query &= Q(donation_due_date__gte=next_donation_date)
-    
-#     donation_requests = DonationRequest.objects.filter(query)
-    
-#     rejected_request_ids = RejectedMatchRequest.objects.filter(
-#         user=request.user
-#     ).values_list('donation_request_id', flat=True)
-    
-#     donation_requests = donation_requests.exclude(id__in=rejected_request_ids)
-    
-#     requests_list = list(donation_requests.values(
-#         'id'
-#     ))
-    
-#     match_request = requests_list[0]
-
-    
-#     return JsonResponse(match_request, status=200)
-    
+from .models import DonationRequest
 
 class DonationRequestViewSet(viewsets.ViewSet):
     swagger_schema = SwaggerAutoSchema
@@ -143,7 +42,7 @@ class DonationRequestViewSet(viewsets.ViewSet):
         
     
     @swagger_auto_schema(responses={200: DonationRequestSerializer})
-    def retrieve(self, request, pk=None):
+    def retrieve(self, pk=None):
         queryset = DonationRequest.objects.all()
         donation_request = get_object_or_404(queryset, pk=pk)
         serializer = DonationRequestSerializer(donation_request)
@@ -155,7 +54,7 @@ class DonationRequestViewSet(viewsets.ViewSet):
     def match(self, request):
         serializer = MatchRequestSerializer(data=request.data)
         if serializer.is_valid():
-            next_donation_date = serializer.data['next_donation_date']
+            next_donation_date = datetime.datetime.strptime(serializer.data['next_donation_date'], '%Y-%m-%d').date()
             # Add a 7-day buffer before and after the donation due date
             queryset = DonationRequest.objects.filter(
                 blood_type=serializer.data['blood_type'],
@@ -170,48 +69,32 @@ class DonationRequestViewSet(viewsets.ViewSet):
                 response_serializer = DonationRequestIdSerializer(queryset.first().id)
                 return Response(response_serializer.data, status=status.HTTP_200_OK)
             else:
-                return Response(status=status.HTTP_404_NOT_FOUND)
+                return Response({"message": "No matching donation requests found"}, status=status.HTTP_404_NOT_FOUND)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
     @swagger_auto_schema(method='post', 
     url_path='match/select',
-    request_body=DonatorRegisteredIdSerializer,
+    request_body=SelectedMatchRequestSerializer,
     responses={201: DonatorRegisteredIdSerializer})
     @action(detail=False, methods=['post'], url_path='match/select')
     def select(self, request):
         """
         Select a match for a donation request
         """
-        # user_id = request.user.id
-        # donation_request_id = request.POST.get('request_id')
-        # donation_request = DonationRequest.objects.get(id=donation_request_id)
-        # # TODO: Return 404 if donation request not found, or object not created
-        # selected_match_request = SelectedMatchRequest.objects.create(user_id, donation_request_id)
-        # donator_registered_id = donation_request.donator_registered_id
-        # return JsonResponse({"donator_registered_id": donator_registered_id}, status=200)
-        
         serializer = SelectedMatchRequestSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
-            # TODO: update donation request with donator_registered_id -> donator_registered.
-            
+            serializer.save()        
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     @swagger_auto_schema(method='post', url_path='match/reject', 
-    request_body=DonatorRegisteredIdSerializer,
+    request_body=RejectedMatchRequestSerializer,
     responses={201: MessageSerializer(data={})})
     @action(detail=False, methods=['post'], url_path='match/reject')
     def reject(self, request):
         """
         Reject a match for a donation request
         """
-        # donator_id = request.user.id
-        # donation_request_id = request.POST.get('request_id')
-        # # TODO: Return 404 if donation request not found, or object not created
-        # rejected_match_request = RejectedMatchRequest.objects.create(donator_id, donation_request_id)
-        # return JsonResponse(status=201)
-        
         serializer = RejectedMatchRequestSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
