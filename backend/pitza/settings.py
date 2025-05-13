3b9a943a5d426fb7e4a3b2a14829cbf2c4c4d77d
Yukyung Lee <ravendesk02@gmail.com>
2025-05-13 20:43:01 +0900

    added omitted parts

--- backend/pitza/settings.py
+++ backend/pitza/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 from pathlib import Path
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -36,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'login',
 ]
 
 MIDDLEWARE = [
