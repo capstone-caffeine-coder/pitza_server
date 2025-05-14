6f72d404b6b0544dfb1b9e82c4bd619522dc5de8
Jiwon Kwon <kjiwon411@gmail.com>
2025-05-14 21:10:45 +0900

    feat: add swagger, user serialiser

--- backend/login/views/user_views.py
+++ backend/login/views/user_views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User  # or your custom user model
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import serializers
+from login.models import User
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'kakao_id', 'nickname','birthdate', 'sex', 'blood_type', 'profile_picture']
+        
+        
+@api_view(['GET'])
+@swagger_auto_schema(responses={200: 'User details retrieved successfully'})
 def user_detail(request, pk):
     user = get_object_or_404(User, pk=pk)
-    return render(request, 'login/user_detail.html', {'user_info': user})
+    serializer = UserSerializer(user)
+
+    return Response(serializer.data, status=200)
