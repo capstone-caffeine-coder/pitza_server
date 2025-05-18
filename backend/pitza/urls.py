bca5a7101323735f7fbebd77850184f8de789686
Yukyung Lee <ravendesk02@gmail.com>
2025-05-18 15:57:30 +0900

    adding api changes to login_3 branch

--- backend/pitza/urls.py
+++ backend/pitza/urls.py
@@ -9,7 +9,9 @@ from rest_framework import routers
 from login import views
 from login.views.auth_views import login_view, login_google, google_callback, login_kakao, kakao_callback
 from login.views.user_views import user_detail  # Import your user detail view
+from login.views.api_views import get_user_by_session_api
 from donations.views import DonationRequestViewSet
+
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -33,16 +35,20 @@ urlpatterns = [
    # project
    path('admin/', admin.site.urls),
    path('chat/', include('chat.urls')),
-    # Auth views
+  
+   # Auth views
    path('', login_view, name='login'),
    path('login/google/', login_google, name='login_google'),
    path('oauth/google/callback/', google_callback, name='google_callback'),
    path('login/kakao/', login_kakao, name='login_kakao'),
    path('oauth/kakao/callback/', kakao_callback, name='kakao_callback'),
    path('profile/setup/', views.profile_setup, name='profile_setup'),
+
    # User info view
-   
    path('user/<int:pk>/', user_detail, name='user_detail'),
+
+   # API views
+   path('get_user_by_session/', get_user_by_session_api, name='get_user_by_session_api'),
 ]
 
 urlpatterns += router.urls
