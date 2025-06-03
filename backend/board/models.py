46fc33b6298c7a9b9643e56b64de2d0739bffc43
ktwozero <kdy020727@naver.com>
2025-06-04 06:01:07 +0900

    헌혈증 게시판 에러 해결 및 axios 추가

--- backend/board/models.py
+++ backend/board/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 
 class DonationPost(models.Model):  # 기부하기 탭
     BLOOD_TYPES = [('A+', 'A+'), ('A-', 'A-'), ('B+', 'B+'), ('B-', 'B-'),
                    ('AB+', 'AB+'), ('AB-', 'AB-'), ('O+', 'O+'), ('O-', 'O-')]
     GENDER_CHOICES = [('M', '남성'), ('F', '여성')]
 
-    donor = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    donor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
     image = models.ImageField(upload_to='donation_images/', blank=True, null=True)
     blood_type = models.CharField(max_length=3, choices=BLOOD_TYPES)
     age = models.PositiveIntegerField()
@@ -18,7 +18,7 @@ class DonationPost(models.Model):  # 기부하기 탭
         return f"[기부] {self.donor.username}"
 
 class RequestPost(models.Model):  # 요청하기 탭
-    requester = models.ForeignKey(User, on_delete=models.CASCADE)
+    requester = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     blood_type = models.CharField(max_length=3, choices=DonationPost.BLOOD_TYPES)
     region = models.CharField(max_length=100)
     reason = models.TextField()
