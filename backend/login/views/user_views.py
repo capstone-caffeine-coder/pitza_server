cf1d6a072b656adec34ef836e31c5cff80310e3d
Yukyung Lee <ravendesk02@gmail.com>
2025-06-07 15:47:45 +0900

    back tousing urls

--- backend/login/views/user_views.py
+++ backend/login/views/user_views.py
@@ -1,3 +1,5 @@
+# login/views/user_views.py
+
 from django.shortcuts import get_object_or_404
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework.decorators import api_view
@@ -8,11 +10,15 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
+    profile_picture = serializers.SerializerMethodField()
+
     class Meta:
         model = User
-        fields = ['id', 'email', 'kakao_id', 'nickname','birthdate', 'sex', 'blood_type', 'profile_picture', 'age']
+        fields = ['id', 'email', 'kakao_id', 'nickname', 'birthdate', 'sex', 'blood_type', 'profile_picture', 'age']
         
-        
+    def get_profile_picture(self, obj):
+        return obj.get_profile_picture_url()
+
 @api_view(['GET'])
 @swagger_auto_schema(responses={200: 'User details retrieved successfully'})
 def user_detail(request, pk):
