ab7b8835689fa347da84d361c478346ea9fa1f53
ktwozero <kdy020727@naver.com>
2025-06-10 02:30:28 +0900

    serializers.py 추가

--- backend/board/serializers.py
+++ backend/board/serializers.py
@@ -2,11 +2,16 @@ from rest_framework import serializers
 from .models import DonationPost, RequestPost
 
 class DonationPostSerializer(serializers.ModelSerializer):
-    donor_username = serializers.CharField(source='donor.username', read_only=True)
+    donor_username = serializers.SerializerMethodField()
 
     class Meta:
         model = DonationPost
         fields = ['id', 'donor_username', 'image', 'blood_type', 'age', 'gender', 'region', 'introduction', 'created_at']
+
+    def get_donor_username(self, obj):
+        if obj.donor:
+            return obj.donor.username
+        return None
 
 class RequestPostSerializer(serializers.ModelSerializer):
     requester_username = serializers.CharField(source='requester.username', read_only=True)
