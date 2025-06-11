5730f31877b415420d137b181c172fa27835dd6f
ktwozero <kdy020727@naver.com>
2025-06-12 02:29:17 +0900

    request 프로필 이미지 추가

--- backend/board/serializers.py
+++ backend/board/serializers.py
@@ -13,7 +13,7 @@ class DonationPostSerializer(serializers.ModelSerializer):
             'id',
             'receiver_id',
             'donor_username',
-            'donor_profile_image',  # 추가
+            'donor_profile_image',
             'image',
             'blood_type',
             'age',
@@ -32,13 +32,13 @@ class DonationPostSerializer(serializers.ModelSerializer):
         if obj.donor and obj.donor.profile_picture_key:
             request = self.context.get('request')
             image_url = obj.donor.profile_picture_key.url
-            # 절대 URL로 변환
             return request.build_absolute_uri(image_url) if request else image_url
         return None
 
 class RequestPostSerializer(serializers.ModelSerializer):
     receiver_id = serializers.IntegerField(source='requester.id', read_only=True)
     requester_username = serializers.CharField(source='requester.nickname', read_only=True)
+    requester_profile_image = serializers.SerializerMethodField()
     image = serializers.ImageField(required=False) 
 
     class Meta:
@@ -46,9 +46,22 @@ class RequestPostSerializer(serializers.ModelSerializer):
         fields = [
             'id', 
             'receiver_id',
-            'requester_username', 
+            'requester_username',
+            'requester_profile_image',
             'image', 
             'blood_type',
             'region', 
             'reason', 
             'created_at']
+        
+        def get_requester_username(self, obj):
+            if obj.requester:
+                return obj.requester.nickname or obj.requester.email or f"Kakao:{obj.requester.kakao_id}"
+            return None
+
+        def get_requester_profile_image(self, obj):
+            if obj.requester and obj.requester.profile_picture_key:
+                request = self.context.get('request')
+                image_url = obj.requester.profile_picture_key.url
+                return request.build_absolute_uri(image_url) if request else image_url
+            return None
